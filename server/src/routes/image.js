1f46b78938969d3d85d4b1ded01212234848ea4e
woochul2 <leewoo0686@gmail.com>
2021-08-30 23:46:56 +0900

    refactor: 이미지 resize로 최적화

--- server/src/routes/image.js
+++ server/src/routes/image.js
@@ -1,22 +1,27 @@
 const aws = require('aws-sdk');
 const express = require('express');
+const sharp = require('sharp');
 
 const router = express.Router();
 const s3 = new aws.S3();
 
-router.get('/:image', async (req, res) => {
+router.get('/:imageName', async (req, res) => {
   try {
     const s3Object = await s3
       .getObject({
         Bucket: 'alexander-photos-images',
-        Key: req.params.image,
+        Key: req.params.imageName,
       })
       .promise();
-    res.writeHead(200, { 'Content-Type': s3Object.ContentType });
-    res.end(s3Object.Body);
-  } catch (error) {
+
+    const { width } = await sharp(s3Object.Body).metadata();
+    const newWidth = req.query.w ? Math.min(parseInt(req.query.w), width) : Math.min(width, 500);
+    const resizedImg = await sharp(s3Object.Body).resize(newWidth).toBuffer();
+    res.contentType(s3Object.ContentType);
+    res.send(resizedImg);
+  } catch (err) {
     res.status(500).json({
-      error,
+      error: err.message,
     });
   }
 });
--- server/src/serverless.js
+++ server/src/serverless.js
@@ -2,7 +2,8 @@ const serverless = require('serverless-http');
 const app = require('./app');
 const Database = require('./Database');
 
-const handler = serverless(app);
+const binaryMimeTypes = { binary: ['image/jpeg', 'image/png'] };
+const handler = serverless(app, binaryMimeTypes);
 module.exports.handler = async (event, context) => {
   await Database.connect();
   const result = await handler(event, context);
