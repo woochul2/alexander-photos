443a465d427b786dc6bbcf192032716e2dc90a4e
woochul2 <leewoo0686@gmail.com>
2022-06-01 23:11:16 +0900

    chore: Lambda 초기 응답 속도 개선
    EventBridge를 이용해 5분마다 lambda에 이벤트를 보내도록 함
    
    참고 자료
    - https://jeonghwan-kim.github.io/dev/2021/04/01/aws-lambda-cold-start.html
    - https://aws.plainenglish.io/preventing-aws-lambda-cold-starts-using-scheduled-event-rules-d468c1681f8

--- server/src/serverless.js
+++ server/src/serverless.js
@@ -8,6 +8,15 @@ const handler = serverless(app, binaryMimeTypes);
 const connectPromise = db.connect();
 
 module.exports.handler = async (event, context) => {
+  if (event['my-type'] === 'Warmer') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
   await connectPromise;
   const result = await handler(event, context);
   return result;
